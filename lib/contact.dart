320b73c0ce8452a508ecbdd140e8c79965923593
Matheus <mrljorge@outlook.com>
2022-07-14 13:06:07 -0300

    moved scroll view outside card

--- lib/contact.dart
+++ lib/contact.dart
@@ -17,71 +17,75 @@ class ContactPageState extends State<ContactPage> {
     return Scaffold(
       appBar: SiteConfig.getAppBar(context, "Contato"),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: SiteConfig.screenHeight * 0.8,
-            width: SiteConfig.screenHeight * 0.8,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              color:
-                  MediaQuery.platformBrightnessOf(context) == Brightness.light
-                      ? Colors.white
-                      : const Color.fromARGB(255, 175, 127, 75).withAlpha(25),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Contato",
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: MediaQuery.platformBrightnessOf(context) ==
-                                    Brightness.light
-                                ? Colors.black
-                                : Colors.white),
-                      ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: SiteConfig.screenHeight * 0.8,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    color: MediaQuery.platformBrightnessOf(context) ==
+                            Brightness.light
+                        ? Colors.white
+                        : const Color.fromARGB(255, 175, 127, 75).withAlpha(25),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Contato",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color:
+                                    MediaQuery.platformBrightnessOf(context) ==
+                                            Brightness.light
+                                        ? Colors.black
+                                        : Colors.white),
+                          ),
+                        ),
+                        const MyTextField(
+                          labelText: 'Nome :',
+                        ),
+                        const MyTextField(
+                          labelText: 'Sobrenome :',
+                        ),
+                        const MyTextField(
+                          labelText: 'Nome da empresa :',
+                        ),
+                        const MyTextField(
+                          labelText: "CNPJ",
+                        ),
+                        const MyTextField(
+                          labelText: 'E-mail corporativo :',
+                        ),
+                        const MyTextField(
+                          labelText: 'Quantidade de funcionarios :',
+                        ),
+                        const MyTextField(
+                          labelText: 'Cidade e estado da empresa :',
+                        ),
+                        const MyTextField(
+                          labelText: 'Mensagem :',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {}, child: const Text("Enviar"))
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    const MyTextField(
-                      labelText: 'Nome :',
-                    ),
-                    const MyTextField(
-                      labelText: 'Sobrenome :',
-                    ),
-                    const MyTextField(
-                      labelText: 'Nome da empresa :',
-                    ),
-                    const MyTextField(
-                      labelText: "CNPJ",
-                    ),
-                    const MyTextField(
-                      labelText: 'E-mail corporativo :',
-                    ),
-                    const MyTextField(
-                      labelText: 'Quantidade de funcionarios :',
-                    ),
-                    const MyTextField(
-                      labelText: 'Cidade e estado da empresa :',
-                    ),
-                    const MyTextField(
-                      labelText: 'Mensagem :',
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                              onPressed: () {}, child: const Text("Enviar"))
-                        ],
-                      ),
-                    ),
-                    SiteConfig.getFooter(),
-                  ],
+                  ),
                 ),
-              ),
+                SiteConfig.getFooter(),
+              ],
             ),
           ),
         ),
