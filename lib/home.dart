0d080444569b23113a5ed8b097e5a7fa43c2429f
Davi Jorge <guimell11@hotmail.com>
2022-07-17 21:26:22 -0300

    home page overhaul

--- lib/home.dart
+++ lib/home.dart
@@ -23,86 +23,125 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 900,
-              width: 3000,
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: Image.asset("assets/images/paula/paulaBanner.jpg"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 117),
-              child: CarouselSlider(
-                options:
-                    CarouselOptions(height: 400.0, enableInfiniteScroll: false),
-                items: [
-                  Image.asset("assets/images/paula/paulaMesa.jpg"),
-                  Image.asset("assets/images/paula/paulaPerfil.jpg"),
-                  Image.asset("assets/images/paula/paulaBanner.jpg"),
-                  Image.asset("assets/images/paula/logoMarca.jpeg")
-                ].map((i) {
+                height: SiteConfig.screenHeight * 0.6,
+                width: SiteConfig.screenWidth,
+                child: Image.asset(
+                  "assets/images/paula/paulaDedo.jpg",
+                  fit: BoxFit.fitWidth,
+                )),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 400.0, enableInfiniteScroll: true, autoPlay: true),
+              items: [
+                const CarouselItem(
+                  items: [
+                    "assets/images/dart_course.png",
+                    "assets/images/paula/paulaMesa.jpg",
+                    "assets/images/paula/paulaPerfil.jpg",
+                    "assets/images/paula/paulaBanner.jpg",
+                  ],
+                ),
+                const CarouselItem(
+                  items: [
+                    "assets/images/paula/paulaMesa.jpg",
+                    "assets/images/paula/paulaPerfil.jpg",
+                    "assets/images/paula/paulaBanner.jpg",
+                    "assets/images/paula/logoMarca.jpeg"
+                  ],
+                )
+              ].map(
+                (i) {
                   return Builder(
                     builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 1),
-                          decoration: const BoxDecoration(
-                              color: Color.fromARGB(255, 216, 45, 45)),
-                          child: i);
+                      return i;
                     },
                   );
-                }).toList(),
-              ),
+                },
+              ).toList(),
             ),
-            Row(
-              children: [
-                SizedBox(
-                  height: SiteConfig.screenHeight * 0.9,
-                  width: SiteConfig.screenWidth * 0.4,
-                  child: FittedBox(
-                    fit: BoxFit.fitHeight,
-                    child: Image.asset("assets/images/paula/paulaPerfil.jpg"),
+            SizedBox(
+              height: SiteConfig.screenHeight * 0.8,
+              width: SiteConfig.screenWidth,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(
+                      "assets/images/paula/paulaPerfil.jpg",
+                      fit: BoxFit.fill,
+                    ),
                   ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                    borderRadius: BorderRadius.circular(20),
-                    color:
-                        const Color.fromARGB(255, 175, 127, 75).withAlpha(200),
-                  ),
-                  height: SiteConfig.screenHeight * 0.2,
-                  width: SiteConfig.screenWidth / 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "Dr.Paula",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Sou Dra Paula Roberta Brasil, graduada em Odontologia há 16 anos e no decorrer desse tempo,"
-                          " venho realizando diversos cursos de atualização,"
-                          " especialização e também mestrado com fins de oferecer o melhor serviço para meus pacientes,"
-                          " ou seja, você que está visitando minha página! \n\n"
-                          "Sou apaixonada pela estética desde minha infância,"
-                          " por isso encontrar a sua melhor versão será um grande prazer e uma imensa alegria. Minha maior ESPECIALIDADE é deixar você mais BONITA e NATURAL,"
-                          " através da HARMONIZAÇÃO OROFACIAL.",
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: SiteConfig.lightColors.primary.withAlpha(25),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Image.asset(
+                                    "assets/images/paula/logoMarca.jpeg",
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(50),
+                                      child: Text(
+                                        "Dr.Paula\nDentista\nEspecialista",
+                                        style: TextStyle(fontSize: 50),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          child: Container(
+                            color: SiteConfig.lightColors.primary.withAlpha(25),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(50),
+                                      child: Text(
+                                        "Sou Dra Paula Roberta Brasil, graduada em Odontologia há 16 anos e no decorrer desse tempo,"
+                                        " venho realizando diversos cursos de atualização,"
+                                        " especialização e também mestrado com fins de oferecer o melhor serviço para meus pacientes,"
+                                        " ou seja, você que está visitando minha página! \n\n"
+                                        "Sou apaixonada pela estética desde minha infância,"
+                                        " por isso encontrar a sua melhor versão será um grande prazer e uma imensa alegria. Minha maior ESPECIALIDADE é deixar você mais BONITA e NATURAL,"
+                                        " através da HARMONIZAÇÃO OROFACIAL.",
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Image.asset(
+                                    "assets/images/paula/paulaVacina.jpg",
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
             SiteConfig.getFooter(),
           ],
